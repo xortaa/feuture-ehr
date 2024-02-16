4c5d1c649578e31514820a4f87ff923cb33c851f
xortaa <kercwinpaul@gmail.com>
2024-02-16 17:32:17 +0800

    social hx minor edit

--- components/SocialHxTable.js
+++ components/SocialHxTable.js
@@ -1,9 +1,9 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from "@chakra-ui/react";
+import { Table, Tbody, Tr, Th, Td, TableContainer, Box } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
-function  SocialHxTable({ socialHxs, onDelete }) {
+function SocialHxTable({ socialHxs, onDelete }) {
   const router = useRouter();
 
   const handleDelete = (id) => {
@@ -39,31 +39,33 @@ function  SocialHxTable({ socialHxs, onDelete }) {
   ];
 
   return (
-    <TableContainer>
-      <Table variant="simple" size="sm">
-        <Tbody>
-          {socialHxs &&
-            socialHxs.length > 0 &&
-            keys.map((key, index) => (
-              <Tr key={index}>
-                <Th>{headers[index]}</Th>
-                <Td>{socialHxs[0][key]}</Td>
-                {index === keys.length - 1 && (
-                  <Td>
-                    <EditIcon
-                      onClick={() => router.push(`/profile/edit/socialHx/${socialHxs[0]._id}`)}
-                      boxSize={5}
-                      mx={1}
-                      cursor="pointer"
-                    />
-                    <DeleteIcon onClick={() => handleDelete(socialHxs[0]._id)} boxSize={5} mx={1} cursor="pointer" />
-                  </Td>
-                )}
-              </Tr>
-            ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <Box boxShadow="base" _hover={{ boxShadow: "lg" }} borderRadius="md" p={2} bgColor="white">
+      <TableContainer>
+        <Table variant="simple" size="sm">
+          <Tbody>
+            {socialHxs &&
+              socialHxs.length > 0 &&
+              keys.map((key, index) => (
+                <Tr key={index}>
+                  <Th>{headers[index]}</Th>
+                  <Td>{socialHxs[0][key]}</Td>
+                  {index === keys.length - 1 && (
+                    <Td>
+                      <EditIcon
+                        onClick={() => router.push(`/profile/edit/socialHx/${socialHxs[0]._id}`)}
+                        boxSize={5}
+                        mx={1}
+                        cursor="pointer"
+                      />
+                      <DeleteIcon onClick={() => handleDelete(socialHxs[0]._id)} boxSize={5} mx={1} cursor="pointer" />
+                    </Td>
+                  )}
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 export default SocialHxTable;
